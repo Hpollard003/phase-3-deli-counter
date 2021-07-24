497ae8a5111a45f276bc13819046dad8a5f841f8
Hpollard003 <hpollard003@gmail.com>
2021-07-23 22:41:15 -0400

    commit tree e8c5da73ca975a8e0a6a3c8e5e724610d304df9b

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def line(katz_deli)
+    if katz_deli.length == 0
+        puts "The line is currently empty."
+    else 
+        customers_present = "The line is currently:"
+        katz_deli.each.with_index(1) do |human, ind|
+            customers_present << " #{ind}. #{human}"
+        end
+
+        puts customers_present
+    end
+end
+
+def take_a_number(katz_deli, new_custo)
+    katz_deli.push(new_custo)
+    puts "Welcome, #{new_custo}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(deli)
+    if deli.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{deli.first}."
+        deli.shift
+    end
+end
+    
